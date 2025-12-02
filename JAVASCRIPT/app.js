152ce845f33935faf9418124e76face722323423
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2025-12-02 22:04:21 +0530

    JavaScript

---
+++ JAVASCRIPT/app.js
@@ -0,0 +1,7 @@
+console.log("Hy this is Abhishek.");
+console.log("How are you all.");
+let a=10;
+let b=20;
+// console.log("Sum of " + a + " and "+ b +" is = " +(a+b));
+//template literals without using many operators
+console.log(`The sum of ${a} and ${b} is = ${a+b}`)
