142603839f6338ede5cf944a246f3103312c03fe
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2025-12-08 21:59:16 +0530

    practice-QS

---
+++ JAVASCRIPT/practiceQ.js
@@ -0,0 +1,39 @@
+/// QS- We can say "good string" is the first lettre of string is equls to 'a' and the lenth of string is greter then 3.
+
+// let str="apple";
+
+// if(str[0]==='a' && str.length>3){
+//     console.log("It is a good String");
+//     }
+//     else{
+//     console.log("It is not a good String");
+
+//     }
+
+let day="5";
+
+switch(day){
+        case "0":
+        console.log("Sunday");
+        break;
+        case "1":
+        console.log("Monday");
+        break;
+        case "2":
+        console.log("Tuesday");
+        break;
+        case "3":
+        console.log("Wednessday");
+        break;
+        case "4":
+        console.log("Thursday");
+        break;
+        case "5":
+        console.log("Friday");
+        break;
+        case "6":
+        console.log("Saturday");
+        break;
+        default:
+            console.log("Wrong input");
+}
