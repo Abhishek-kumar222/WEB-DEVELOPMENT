52e31bcdd6a6f5ebaec2f46d21112aa512058902
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2025-12-12 20:16:46 +0530

    code

--- JAVASCRIPT/practiceQ2.js
+++ JAVASCRIPT/practiceQ2.js
@@ -1,5 +1,38 @@
 ////Qs-1
 
-let msg="Abhishek";
-let newMsg=msg.trim().toUpperCase()
-console.log(newMsg);
+// let msg="Abhishek";
+// let newMsg=msg.trim().toUpperCase()
+// console.log(newMsg);
+
+///Arrays Practice using shift& unshift
+
+
+
+// let arr=["january", "july", "march","august"];
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// arr.unshift("june");
+// console.log(arr);
+
+// arr.unshift("july");
+// console.log(arr);
+
+
+
+///Arrays Practice using splice 
+
+// let months=["january","july","march","august"];
+// months.splice(0,2,"july","june");
+// console.log(months);
+
+/////qs-5
+
+let lang=["c","c++","html","javascript","python","java","c#","sql"];
+let index=lang.reverse().indexOf("c#");
+console.log(index);
