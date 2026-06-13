54cfc1435e9f6b77cb3c3b44ec995c5b5a80c76a
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-06-13 14:53:53 +0530

    code

--- JS PART 4/loop-with-arr.js
+++ JS PART 4/loop-with-arr.js
@@ -1,5 +1,48 @@
-let fruits = ["mango" , "banana" , "orange" , "apple"];
+// let fruits = ["mango" , "banana" , "orange" , "apple"];
 
-for(let i =0;i<fruits.length;i++){
-    console.log(i , fruits[i]);
-}
+// for(let i =0;i<fruits.length;i++){
+//     console.log(i , fruits[i]);
+// }
+
+
+
+// /nested loop with nasted array
+
+// let bikes = [["platina" , "discover" , "100CC" ] , ["pulsor" , "royal enfeid" , "hf delux"]];
+
+// for(let i = 0;i<bikes.length;i++){
+//     console.log(`arr num ${i+1} : ---`);
+//     for(let j=0;j<bikes[0].length;j++){
+//         console.log(bikes[i][j]);
+//     }
+// }
+
+
+
+////---for of loop---///  using for getting collection of data
+
+// let bikes = ["platina" , "discover" , "100CC" ,"pulsor" , "royal enfeid" , "hf delux"];
+
+//   for (bike of bikes){
+//     console.log(bike);
+//   }
+
+// let name = "Abhishek";
+// for(alphabet of name){
+//     console.log(alphabet);
+// } 
+
+
+///----nested for of loop with nasted array
+
+let bikes = [["platina" , "discover" , "100CC" ] , ["pulsor" , "royal enfeid" , "hf delux"]];
+
+ for (bike of bikes){
+     console.log(bike);
+ }
+ //or
+  for (bike of bikes){
+     for (names of bike){
+     console.log(names);
+     }
+ }
---
+++ JS PART 4/practice -qs.js
@@ -0,0 +1,44 @@
+///qs 1
+
+// let arr = [1,2,3,4,5,6,2,3];
+// let num = prompt("Enter number that you want to remove from arr ");
+
+// for(let i = 0; i<arr.length;i++){
+//     if(arr[i] == num){
+//         arr.splice(i,1);
+//         i--;
+//     }
+//     else{
+//     console.log(arr[i]);
+//     }
+// }
+
+///qs2
+// let numbers = prompt("Enter number ");
+// count = 0
+// for(number of numbers){
+//     count++;
+// }
+//     console.log(`no. of dizit - ${count}`);
+
+
+    ///qs3
+
+// let numbers = prompt("Enter number ");
+// sum = 0
+// for(number of numbers){
+//     sum+=parseInt(number);
+// }
+//     console.log(`sum of dizit - ${sum}`);
+
+///qs4 - largest number 
+
+let arr = [1,10,3,4,9,6,2,3,5,7,12,50,5,];
+let largest =0 ;
+    for(let j=0; j<arr.length; j++){
+        if(arr[j]>largest){
+            largest=arr[j];
+        }
+    }
+ console.log(largest);
+
