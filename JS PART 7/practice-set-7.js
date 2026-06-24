5b0aefbf65586c8edde13f037d43c5705bbfa972
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-06-24 23:27:38 +0530

    code

---
+++ JS PART 7/practice-set-7.js
@@ -0,0 +1,46 @@
+// //1. calculate average of array using arrow function 
+
+// const arrayAverage = (arr) => {
+//     let sum=0;
+//     for(let element of arr){
+//       sum+=element;
+//     }
+//     return sum / arr.length;
+// };
+
+// let arr = [1,2,3];
+// console.log(arrayAverage(arr));
+
+// //2.check even or not 
+
+// let isEven = n => n%2==0;
+
+// //3.
+// const object = {
+//     message: "Hello World",
+
+//     logMessage() {
+//         console.log(this.message);
+//     }
+// };
+
+// setTimeout((object.logMessage), 1000);
+
+// //4.
+
+// let length = 4;
+// function callback() {
+//     console.log(this.length); 
+// }
+
+// const object ={
+//     length: 5,
+//     method(callback) {
+//         callback();
+//     }
+// };
+
+// object.method(callback,1,2);
+
+console.log(a);
+const a;
