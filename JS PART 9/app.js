4e2df445806339efd78a9450fa7a94ea8d792903
Abhishek Kumar <choudharyabhishek40739@gmail.com>
2026-06-27 18:44:02 +0530

    code

---
+++ JS PART 9/app.js
@@ -0,0 +1,18 @@
+// let smallImg = document.getElementsByClassName("oldImg");
+// for (let i=0 ; i<smallImg.length;i++){
+//     smallImg[i].src = "assets/spiderman_img.png";
+//     console.log('src has changed of img index no. ',[i])
+// }
+// console.log(smallImg.length);
+
+////queryselector
+
+// console.log(document.querySelector("h1"));
+// console.log(document.querySelector("#description"));
+// console.log(document.querySelector(".oldImg"));
+// console.dir(document.querySelector("div a"));
+// console.dir(document.querySelectorAll("div a"));
+// console.dir(document.querySelectorAll("p"));
+
+let para = document.querySelector("p");
+console.dir(para);
